1f1a39c936db87ea5c75e99fccca8e1092a39671
rudeous13 <Vp1311Freelance@gmail.com>
2026-04-04 01:53:15 +0530

    Invoice fixed

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,25 @@
 # Import file or packages
 from django.urls import path
 from . import views
+from orders import views as orders_views
 
 # App name
 app_name = "core"
 
 # All urls of core app
 urlpatterns = [
-  path('', views.homepage, name="homepage"),
-  path('prebuilt', views.prebuilt, name="prebuilt"),
-  path('built', views.built, name="built"),
-  path('accessories', views.accessories, name="accessories"),
-  path('cart', views.cart, name="cart"),
-  path('profile', views.profile, name="profile"),
+    path('', views.homepage, name="homepage"),
+    path('prebuilt', views.prebuilt, name="prebuilt"),
+    path('built', views.built, name="built"),
+    path('accessories', views.accessories, name="accessories"),
+    path('cart', views.cart, name="cart"),
+    path('profile', views.profile, name="profile"),
 
 
-  path('profile/builds/', views.pro_builds, name="pro_builds"),
-  path('profile/order', views.pro_order, name="pro_order"),
-  path('profile/security', views.pro_security, name="pro_security"),
-  path('profile/setting', views.pro_setting, name="pro_setting")
+    path('profile/builds/', views.pro_builds, name="pro_builds"),
+    path('profile/order', views.pro_order, name="pro_order"),
+    path('profile/order/<int:order_id>/receipt/',
+         orders_views.invoice_view, name='order_receipt'),
+    path('profile/security', views.pro_security, name="pro_security"),
+    path('profile/setting', views.pro_setting, name="pro_setting")
 ]
